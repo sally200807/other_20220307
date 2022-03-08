c5fe46ce4d5c15cdbacd68565690ebfde742778d
sally200807 <sally200807@gmail.com>
2022-03-08 16:25:08 +0800

    add bmi

---
+++ BMI_20220307.py
@@ -0,0 +1,19 @@
+height = int(input('請輸入身高(cm)'))
+weight = input('請輸入體重(kg)')
+weight = int(weight)
+height = height / 100 
+bmi = weight / height **2
+if bmi < 18.5:
+	print ('你的BMI值', bmi, '體重過輕')
+elif bmi >= 18.5 and bmi < 24:
+	print('你的BMI值', bmi, '正常')
+elif bmi >= 24 and bmi < 27:
+	print('你的BMI值', bmi, '稍重')
+elif bmi >= 27 and bmi < 30:
+	print('你的BMI值', bmi, '輕度肥胖')
+elif bmi >= 30 and bmi < 35:
+	print('你的BMI值', bmi, '中度肥胖')
+#elif bmi > 35:
+	#print('你的BMI值', bmi, '重度肥胖')
+else :
+	print('你的BMI值', bmi, '重度肥胖')
